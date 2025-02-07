13d59efb51812df728511f94e56be18608ccab0f
Lopesmatheus1 <lopesmatheus1@hotmail.com>
2025-02-07 09:57:05 -0300

    feat: add inital nextauth login setup

---
+++ app/_lib/auth.ts
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { db } from "./prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [Google],
+});
